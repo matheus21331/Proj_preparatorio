3f2ced4bbd9777c7bbfe546982f397d914053208
Matheus <matheusbernardi@santins.com.br>
2018-10-17 13:39:37 -0300

    Commit changes

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+    
+    // Slick Banner Home
+    $('.banner-carousel').slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });    
+});
